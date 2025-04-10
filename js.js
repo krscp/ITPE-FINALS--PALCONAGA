11747ae27697df5e2beed76b3c0838288cf93589
krscp <mosquedaemanuel050206@gmail.com>
2025-04-10 12:47:04 +0800

    Add files via upload

---
+++ js.js
@@ -0,0 +1,33 @@
+document.addEventListener('DOMEContentLoaded' , function(){
+cons registrationform=
+
+document.getElementby(Students Registration):
+document .setItem("Students Id")value;
+document .setItem("First Name")value;
+document .setItem ("Last Name")value;
+document .setItem("Courses");value;
+document .setItem("Birthdate")value;
+document .setItem("Contact Number")value;
+document .setItem("Username")value;
+document .setItem("Password");value;
+
+local storage .getElementby ("Students Id", Students Id);
+local storage .getElementby ("Last Name", First Name);
+local storage .getElementby ("First Name", Last Name);
+local storage .getElementby ("Courses", Courses);
+local storage .getElementby ("Birth Date", Birth Date);
+local storage .getElementby ("Contact Number", Contact Number);
+local storage .getElementby ("Username", Username);
+local storage .getElementby ("Password", Password);
+
+<link href="index.html">
+
+
+
+
+
+
+
+
+
+
